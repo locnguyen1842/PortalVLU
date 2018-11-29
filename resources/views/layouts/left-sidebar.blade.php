68310503326b8dcd261644f89f0834dc0bb654c4
Loc Nguyen <haimuoibon024@gmail.com>
2018-11-29 12:41:22 +0700

    add master view , css , js

---
+++ resources/views/layouts/left-sidebar.blade.php
@@ -0,0 +1,35 @@
+<div id="cm-menu">
+    <nav class="cm-navbar cm-navbar-primary">
+        <div class="cm-flex"><a href="index.html" class="cm-logo"></a></div>
+        <div class="btn btn-primary md-menu-white" data-toggle="cm-menu"></div>
+    </nav>
+    <div id="cm-menu-content">
+        <div id="cm-menu-items-wrapper">
+            <div id="cm-menu-scroller">
+                <ul class="cm-menu-items">
+                    <li><a href="index.html" class="sf-house">Home</a></li>
+                    <li><a href="dashboard-sales.html" class="sf-dashboard">Dashboard</a></li>
+                    <li><a href="components-text.html" class="sf-brick">Components</a></li>
+                    <li class="cm-submenu pre-open">
+                        <a class="sf-window-layout">Navbar layouts <span class="caret"></span></a>
+                        <ul>
+                            <li><a href="layouts-breadcrumb1.html">1st nav breadcrumb</a></li>
+                            <li class="active"><a href="layouts-breadcrumb2.html">2nd nav breadcrumb</a></li>
+                            <li><a href="layouts-tabs.html">2nd nav tabs</a></li>
+                        </ul>
+                    </li>
+                    <li class="cm-submenu">
+                        <a class="sf-cat">Icons <span class="caret"></span></a>
+                        <ul>
+                            <li><a href="ico-sf.html">Small-n-flat</a></li>
+                            <li><a href="ico-md.html">Material Design</a></li>
+                            <li><a href="ico-fa.html">Font Awesome</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="notepad.html" class="sf-notepad">Text Editor</a></li>
+                    <li><a href="login.html" class="sf-lock-open">Login page</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
