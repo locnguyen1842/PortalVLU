7f42ddd12646b0b96be48848cfc58674d95ff714
syphule <syphule1596@gmail.com>
2019-03-25 01:21:09 +0700

    Create template for sprint SB

---
+++ resources/views/admin/sb/sb-print.blade.php
@@ -0,0 +1,514 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="wrap">
+        <!-- begin header -->
+        <div class="header">
+            <div class="headerLeft">
+                <p>BỘ GIÁO DỤC VÀ ĐÀO TẠO</p>
+                <h4>TRƯỜNG ĐẠI HỌC VĂN LANG</h4>
+            </div>
+            <div class="headerRight">
+                <h4>CỘNG HÒA XÃ HỘI CHỦ NGHĨA VIỆT NAM</h4>
+                <h4 class="hr2">Độc lập-Tự do- Hạnh phúc</h4>
+            </div>
+            <div class="clear"></div>
+        </div>
+        <!-- END HEADER -->
+        <!-- begin tieu de -->
+        <div class="tieuDe">
+            <h3>LÝ LỊCH KHOA HỌC</h3>
+        </div>
+        <!-- end tieu de -->
+        <!-- begin noi dung 1 -->
+        <div class="content">
+            <div class="form-group header-content1">
+                <div class="form-group-sub1">
+                    <h3 for="">I. LÝ LỊCH SƠ LƯỢC</h3>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="form-group-sub1">
+                    <label for="">Họ và tên:</label>
+                </div>
+                <div class="form-group-sub2">
+                    <label for="">Giới tính:</label>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="form-group-sub1">
+                    <label for="">Ngày,tháng,năm sinh</label>
+                </div>
+                <div class="form-group-sub2">
+                    <label for="">Nơi sinh:</label>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="form-group-sub1">
+                    <label for="">Quê quán:</label>
+                </div>
+                <div class="form-group-sub2">
+                    <label for="">Dân tộc:</label>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="form-group-sub1">
+                    <label for="">Học vị cao nhất:</label>
+                </div>
+                <div class="form-group-sub2">
+                    <label for="">Năm,nước nhận học vị:</label>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="form-group-sub1">
+                    <label for="">Chức danh khoa học cao nhất:</label>
+                </div>
+                <div class="form-group-sub2">
+                    <label for="">Năm bổ nhiệm:</label>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="form-group-sub1">
+                    <label for="">Chức vụ(hiện tại hoặc trước khi nghỉ hưu):</label>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="form-group-sub1">
+                    <label for="">Chỗ ở riêng hoặc địa chỉ liên lạc:</label>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="form-group-sub1" style="float: left;width: 50%;">
+                    <label for="">Điện thoại liên hệ: CQ:</label>
+                </div>
+                <div class="form-group-sub2" style="float: left;width: 25%;">
+                    <label for="">NR:</label>
+                </div>
+                <div class="form-group-sub3" style="float: left;width: 25%;">
+                    <label for="">DĐ:</label>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="form-group-sub1">
+                    <label for="">Fax:</label>
+                </div>
+                <div class="form-group-sub2">
+                    <label for="">Email:</label>
+                </div>
+            </div>
+
+        </div>
+        <!-- end noi dung 1 -->
+        <!-- begin noi dung 2 -->
+        <div class="content">
+            <div class="form-group">
+                <div class="form-group-sub1 header-content">
+                    <h3 for="">II. QUÁ TRÌNH ĐÀO TẠO</h3>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="form-group-sub1">
+                    <h4 for="">1. Đại học</h4>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="form-group-sub1">
+                    <label for="">Hệ đào tạo:</label>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="form-group-sub1">
+                    <label for="">Nơi đào tạo:</label>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="form-group-sub1">
+                    <label for="">Ngành học:</label>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="form-group-sub1">
+                    <label for="">Nước đào tạo:</label>
+                </div>
+                <div class="form-group-sub2">
+                    <label for="">Năm tốt nghiệp:</label>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="form-group-sub1">
+                    <label for="">Bằng đại học 2:</label>
+                </div>
+                <div class="form-group-sub2">
+                    <label for="">Năm tốt nghiệp:</label>
+                </div>
+            </div>
+            <div class="clear"></div>
+            <div class="form-group">
+                <div class="form-group-sub1">
+                    <h4 for="">2. Sau đại học</h4>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="form-group-sub1">
+                    <ul class="dash">
+                        <li>
+                            <label for="">Thạc sĩ chuyên ngành:</label>
+                        </li>
+                    </ul>
+                    <!-- <label for="">-Thạc sĩ chuyên ngành:</label> -->
+                </div>
+                <div class="form-group-sub2">
+                    <label for="">Năm cấp bằng:</label>
+                </div>
+                <div class="form-group-sub2">
+                    <label for="">Nơi đào tạo:</label>
+                </div>
+            </div>
+            <br>
+            <div class="clear"></div>
+            <div class="form-group">
+                <div class="form-group-sub1">
+                    <ul class="dash">
+                        <li>
+                            <label for="">Tiến sĩ chuyên ngành:</label>
+                        </li>
+                    </ul>
+                    <!-- <label for="">-Thạc sĩ chuyên ngành:</label> -->
+                </div>
+                <div class="form-group-sub2">
+                    <label for="">Năm cấp bằng:</label>
+                </div>
+                <div class="form-group-sub2">
+                    <label for="">Nơi đào tạo:</label>
+                </div>
+            </div>
+            <div class="clear"></div>
+            <div class="form-group">
+                <div class="form-group-sub1">
+                    <ul class="dash">
+                        <li>
+                            <label for="">Tên luận án:</label>
+                        </li>
+                    </ul>
+                    <!-- <label for="">-Thạc sĩ chuyên ngành:</label> -->
+                </div>
+            </div>
+
+            <div class="form-group">
+                    <div class="form-group-sub1">
+                        <h4 for="">3. Ngoại ngữ</h4>
+                    </div>
+                </div>
+            <div class="form-group">
+                    <div class="form-group-sub1">
+                            <ul style="list-style-type: none">
+                                <li>
+                                    <label for="">1.</label>
+                                </li>
+                            </ul>
+                            <!-- <label for="">-Thạc sĩ chuyên ngành:</label> -->
+                        </div>
+                        <div class="form-group-sub2">
+                            <label for="">Mức độ sử dụng:</label>
+                        </div>
+
+            </div>
+            <div class="form-group">
+                    <div class="form-group-sub1">
+                            <ul style="list-style-type: none">
+                                <li>
+                                    <label for="">2.</label>
+                                </li>
+                            </ul>
+                            <!-- <label for="">-Thạc sĩ chuyên ngành:</label> -->
+                        </div>
+                        <div class="form-group-sub2">
+                            <label for="">Mức độ sử dụng:</label>
+                        </div>
+
+            </div>
+
+        </div>
+        <!-- begin noi dung 3 -->
+        <div class="content">
+            <div class="form-group">
+                    <div class="form-group-sub1 header-content">
+                            <h3 for="">III. QUÁ TRÌNH CÔNG TÁC CHUYÊN MÔN</h3>
+                        </div>
+            </div>
+
+
+            <table class="table-content" border="1px" cellspacing="0px">
+                <tr>
+                    <td align="center">
+                        <strong>Thời gian</strong>
+                    </td>
+                    <td align="center">
+                        <strong>Nơi công tác </strong>
+                    </td>
+                    <td align="center">
+                        <strong>Công việc đảm nhiệm</strong>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </table>
+
+        </div>
+        <!-- end nội dung 3 -->
+        <!-- begin nội dung 4 -->
+        <div class="content">
+            <div class="form-group">
+                    <div class="form-group-sub1 header-content">
+                            <h3 for="">IV. QUÁ TRÌNH NGHIÊN CỨU KHOA HỌC</h3>
+                        </div>
+            </div>
+            <div class="form-group">
+                <div class="form-group-sub1 " style="margin-top:1rem">
+
+                        <h4>
+                                1. Các đề tài nghiên cứu khoa học đã và đang tham gia
+                            </h4>
+                </div>
+
+            </div>
+            <table class="table-content" border="1px" cellspacing="0px">
+                <tr>
+                    <td align="center" width="50px">
+                        <strong>TT</strong>
+                    </td>
+                    <td align="center" width="250px">
+                        <strong>Tên đề tài nghiên cứu </strong>
+                    </td>
+                    <td align="center">
+                        <strong>Năm bắt đầu/Năm hoàn thành</strong>
+                    </td>
+                    <td align="center">
+                        <strong>Đề tài cấp (NN, Bộ, ngành, trường)</strong>
+                    </td>
+                    <td align="center">
+                        <strong>Trách nhiệm tham gia trong đề tài</strong>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </table>
+            <div class="form-group">
+                    <div class="form-group-sub1" style="margin-top:1rem">
+
+                            <h4>
+                                    2. Các công trình khoa học đã công bố
+                                </h4>
+                    </div>
+            </div>
+            <table class="table-content" border="1px" cellspacing="0px">
+                <tr>
+                    <td align="center" width="50px">
+                        <strong>TT</strong>
+                    </td>
+                    <td align="center" width="250px">
+                        <strong>Tên công trình </strong>
+                    </td>
+                    <td align="center">
+                        <strong>Năm công bố</strong>
+                    </td>
+                    <td align="center"width="300px">
+                        <strong>Tên tạp chí</strong>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </table>
+
+        </div>
+        <!-- end noi dung4 -->
+        <!-- nội dung kí  -->
+        <div class="content">
+            <div class="form-group" style="margin-left: 60%">
+                <span>.............,</span>
+                <span style="">ngày</span>
+                <span style="margin-left: 2rem">tháng</span>
+                <span style="margin-left: 2rem">năm</span>
+            </div>
+            <div class="form-group" style="margin-left: 67% ; margin-top: 0.7rem">
+                <span><strong>Người khai kí tên</strong></span>
+            </div>
+            <div class="form-group" style="margin-left: 63% ; margin-top: 0.5rem">
+                <span>(Ghi rõ chức danh,học vị)</span>
+            </div>
+        </div>
+        <!-- end noi dung kí -->
+    </div>
+</body>
+
+</html>
+<style>
+*{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+@media print
+{
+.header-content {page-break-before:always}
+}
+.clear{ clear: both;}
+.wrap{
+    width: 100%;
+    min-height: 2000px;
+    margin: auto;
+    margin-top:20px ;
+    overflow: hidden;
+    background-color: white;
+}
+.header{
+    width:100%;
+    margin-top: 4rem;
+    margin-left: 5rem;
+}
+.headerLeft{
+    float: left;
+    width: 50%;
+}
+.headerLeft>p{
+    padding-left: 2rem;
+    font-size: 15px;
+
+}
+.headerRight{
+    float: left;
+    width: 50%;
+
+}
+.hr2{
+    padding-left: 5rem;
+}
+/* begin tieu de */
+.tieuDe{
+    text-align: center;
+    margin-top: 3rem;
+}
+/* end tieu de */
+/* BEGIN NOI DUNG 1 */
+.content{
+    width: 90%;
+    margin: auto;
+    margin-top: 2rem;
+    margin-left : 5rem;
+}
+.form-group{
+    width: 100%;
+}
+.form-group-sub1{
+    float: left;
+    width: 55%;
+    margin: 5px 0px;
+}
+.form-group-sub2{
+    width: 45%;
+    float: left;
+    margin: 5px 0px;
+}
+
+.form-group ol{
+    font-weight: 700;
+}
+
+.table-content{
+    margin-top:1rem;
+    width: 95%;
+    height: 200px;
+}
+
+
+.form-group span{
+    font-size: 17px;
+
+}
+ul.dash {
+    list-style: none;
+    margin-left: 0;
+    padding-left: 1em;
+}
+ul.dash > li:before {
+    display: inline-block;
+    content: "-";
+    width: 1em;
+    margin-left: -1em;
+}
+</style>
